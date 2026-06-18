63fce0f4e1f66ecb5c699c67a51ff804091fb9e4
sandhyasriram <136562735+sandhyasriram@users.noreply.github.com>
2026-06-18 13:00:39 +0530

    Delete queries/yearly_order_trends.sql

--- queries/yearly_order_trends.sql
+++
@@ -1,8 +0,0 @@
-select
-   extract(year from order_purchase_timestamp) as order_year,
-   count(order_id) as total_orders
-   from target.orders
-   group by order_year
-   order by order_year
-
-  
